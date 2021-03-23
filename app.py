218a28aa6c043a4215baf83193a75a60b32dd673
ThThelitz <thilo.thelitz@gmail.com>
2021-03-23 09:55:41 +0100

    Add input and client-server communication

--- app.py
+++ app.py
@@ -5,13 +5,34 @@ Basic code explained here:
 https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
 Use terminal command "flask run" for dev server
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route('/')
+@app.route("/")
 def main():
     return render_template("main.html")
 
-@app.route('/gaps')
+@app.route("/gaps")
 def gaps():
     return render_template("gaps.html")
+
+@app.route("/bundle", methods=["GET"])
+def bundle():
+    bundle = test_bundle
+    return json.dumps(bundle)
+
+test_bundle = {"target": "fish",
+               "sent_1_left": "Andrea got up early in the morning to",
+               "sent_1_gap": "fish",
+               "sent_1_right": ".",
+               "sent_2_left": "If you can see this, you're a good programmer and not a",
+               "sent_2_gap": "fish",
+               "sent_2_right": ".",
+               "sent_3_left": "Andrea got up early in the morning to",
+               "sent_3_gap": "fish",
+               "sent_3_right": ".",
+               "sent_4_left": "Andrea got up early in the morning to",
+               "sent_4_gap": "fish",
+               "sent_4_right": "."}
---
+++ static/scripts.js
@@ -0,0 +1,70 @@
+targetWord = "care";
+
+// Does something everytime something is input into the guess field, typing
+// or copy-pasting or text-to-speech input
+//guess.oninput = isCorrectGuess();
+
+async function isCorrectGuess() {
+    if (guess.value.toLowerCase() === targetWord) {
+        alert("Wow you're fucking amazing");
+        sleep(1000)
+        getNewBundle()
+    }
+}
+
+async function getNewBundle() {
+    let url = "/bundle";
+    let response = await fetch(url, {method: "GET"})
+    // Check for promise and http errors
+    .catch(err => alert("Error b/c of promise rejection: ".concat(err)));
+
+    if (response.ok) {
+        // Parses json into js object
+        bundle = await response.json();
+    }
+    else {
+        alert("HTTP-Error: " + response.status);
+    }
+
+    // Expected JSON structure example:
+    // bundle.target = "care"
+    // bundle.sent_1_left = "I don't"
+    // bundle.sent_1_gap = "care"
+    // bundle.sent_1_right = "!"
+    // And so on for all 4 sentences
+
+    targetWord = bundle.target
+
+    sent1Left = document.getElementById("sent_1_left")
+    sent1Left.innerHTML = bundle.sent_1_left
+    sent1Gap = document.getElementById("sent_1_gap")
+    sent1Gap.innerHTML = bundle.sent_1_gap
+    sent1Right = document.getElementById("sent_1_right")
+    sent1Right.innerHTML = bundle.sent_1_right
+
+    sent2Left = document.getElementById("sent_2_left")
+    sent2Left.innerHTML = bundle.sent_2_left
+    sent2Gap = document.getElementById("sent_2_gap")
+    sent2Gap.innerHTML = bundle.sent_2_gap
+    sent2Right = document.getElementById("sent_2_right")
+    sent2Right.innerHTML = bundle.sent_2_right
+
+    sent3Left = document.getElementById("sent_3_left")
+    sent3Left.innerHTML = bundle.sent_3_left
+    sent3Gap = document.getElementById("sent_3_gap")
+    sent3Gap.innerHTML = bundle.sent_3_gap
+    sent3Right = document.getElementById("sent_3_right")
+    sent3Right.innerHTML = bundle.sent_3_right
+
+    sent4Left = document.getElementById("sent_4_left")
+    sent4Left.innerHTML = bundle.sent_4_left
+    sent4Gap = document.getElementById("sent_4_gap")
+    sent4Gap.innerHTML = bundle.sent_4_gap
+    sent4Right = document.getElementById("sent_4_right")
+    sent4Right.innerHTML = bundle.sent_4_right
+}
+
+function sleep(ms) {
+    // From https://stackoverflow.com/a/39914235
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
