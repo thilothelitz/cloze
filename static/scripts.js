93baa705f7a77fa86c639954b810fbf66a878f69
ThThelitz <thilo.thelitz@gmail.com>
2021-04-06 08:25:54 +0200

    Try adding new bundle as new element to DOM
    This just takes one row from the bundle and adds it as a new element
    to the DOM via javascript.

--- static/scripts.js
+++ static/scripts.js
@@ -33,35 +33,66 @@ async function getNewBundle() {
     // bundle.sent_1_right = "!"
     // And so on for all 4 sentences
 
-    targetWord = bundle.target
+    targetWord = bundle.target;
 
-    sent1Left = document.getElementById("sent_1_left")
-    sent1Left.innerHTML = bundle.sent_1_left
-    sent1Gap = document.getElementById("sent_1_gap")
-    sent1Gap.innerHTML = bundle.sent_1_gap
-    sent1Right = document.getElementById("sent_1_right")
-    sent1Right.innerHTML = bundle.sent_1_right
+    bundleDiv = document.createElement("div");
+    bundleDiv.className = "bundle";
+    document.append(bundleDiv)
 
-    sent2Left = document.getElementById("sent_2_left")
-    sent2Left.innerHTML = bundle.sent_2_left
-    sent2Gap = document.getElementById("sent_2_gap")
-    sent2Gap.innerHTML = bundle.sent_2_gap
-    sent2Right = document.getElementById("sent_2_right")
-    sent2Right.innerHTML = bundle.sent_2_right
+    rowDiv = document.createElement("div");
+    rowDiv.className = "row";
+    bundleDiv.append(rowDiv)
 
-    sent3Left = document.getElementById("sent_3_left")
-    sent3Left.innerHTML = bundle.sent_3_left
-    sent3Gap = document.getElementById("sent_3_gap")
-    sent3Gap.innerHTML = bundle.sent_3_gap
-    sent3Right = document.getElementById("sent_3_right")
-    sent3Right.innerHTML = bundle.sent_3_right
+    colDiv = document.createElement("div");
+    rowDiv.className = "col left";
+    rowDiv.innerHTML = bundle.sent_1_left;
+    rowDiv.append(colDiv)
 
-    sent4Left = document.getElementById("sent_4_left")
-    sent4Left.innerHTML = bundle.sent_4_left
-    sent4Gap = document.getElementById("sent_4_gap")
-    sent4Gap.innerHTML = bundle.sent_4_gap
-    sent4Right = document.getElementById("sent_4_right")
-    sent4Right.innerHTML = bundle.sent_4_right
+    colDiv = document.createElement("div");
+    rowDiv.className = "col-1 gap";
+    rowDiv.innerHTML = bundle.sent_1_gap;
+    rowDiv.append(colDiv)
+
+    colDiv = document.createElement("div");
+    rowDiv.className = "col right";
+    rowDiv.innerHTML = bundle.sent_1_right;
+    rowDiv.append(colDiv)
+
+    // document.append(bundleDiv)
+    // bundleDiv.append(rowDiv)
+    // rowDiv.append(leftColDiv)
+    // rowDiv.append(midColDiv)
+    // rowDiv.append(rightColDiv)
+
+    
+
+    // sent1Left = document.getElementById("sent_1_left")
+    // sent1Left.innerHTML = bundle.sent_1_left
+    // sent1Gap = document.getElementById("sent_1_gap")
+    // sent1Gap.innerHTML = bundle.sent_1_gap
+    // sent1Right = document.getElementById("sent_1_right")
+    // sent1Right.innerHTML = bundle.sent_1_right
+
+    // sent2Left = document.getElementById("sent_2_left")
+    // sent2Left.innerHTML = bundle.sent_2_left
+    // sent2Gap = document.getElementById("sent_2_gap")
+    // sent2Gap.innerHTML = bundle.sent_2_gap
+    // sent2Right = document.getElementById("sent_2_right")
+    // sent2Right.innerHTML = bundle.sent_2_right
+
+    // sent3Left = document.getElementById("sent_3_left")
+    // sent3Left.innerHTML = bundle.sent_3_left
+    // sent3Gap = document.getElementById("sent_3_gap")
+    // sent3Gap.innerHTML = bundle.sent_3_gap
+    // sent3Right = document.getElementById("sent_3_right")
+    // sent3Right.innerHTML = bundle.sent_3_right
+
+    // sent4Left = document.getElementById("sent_4_left")
+    // sent4Left.innerHTML = bundle.sent_4_left
+    // sent4Gap = document.getElementById("sent_4_gap")
+    // sent4Gap.innerHTML = bundle.sent_4_gap
+    // sent4Right = document.getElementById("sent_4_right")
+    // sent4Right.innerHTML = bundle.sent_4_right
 }
 
 function sleep(ms) {
